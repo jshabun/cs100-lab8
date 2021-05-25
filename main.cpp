e36bfc39cd6af288b2b5ce1df96314eed80e5103
jshabun <jshab003@ucr.edu>
2021-05-25 07:43:20 -0700

    main and iterator_test

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ int main(int argc, char** argv){
 	if (test) {
 		cout << test->stringify() << " = " << test->evaluate() << endl;
 		cout << "Latex: \n" << print_latex(test) << endl;
-		cout << "MathML: \n" << print_MathML(test) << enl;
+		cout << "MathML: \n" << print_mathML(test) << enl;
 	} else {
 		cout << "Error: Invalid" << endl;
 	}
 
-	delete factory_patternl
+	delete factory_pattern;
 	delete test;	
 
 	return 0;
