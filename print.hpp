1accc3b9f22b2f3b1a1deb77e6b37785a07a2a7e
Aguyam1 <jumpman5.wm@gmail.com>
2021-05-25 06:11:38 -0700

    Prints out LaTeX and mathML

---
+++ print.hpp
@@ -0,0 +1,41 @@
+#ifndef __PRINT_HPP__
+#define __PRINT_HPP__
+
+#include "visitorLaTeX.hpp"
+#include "visitorMathML.hpp"
+#include "iterator.hpp"
+#include <string>
+
+using namespace std;
+
+string print_latex(Base* ptr) {
+	VisitorLaTeX* visit = new VisitorLaTeX();
+	Iterator iter(ptr);
+	
+	while(!iter.is_done()) {
+		iter.current_node()->accept(visit, iter.current_index());
+		iter.next();
+	}
+	
+	string result;
+	result += "$" + visit->getString() + "$";
+	delete visit;
+	return result;
+}
+
+string print_mathML(Base* ptr) {
+	VisitorMathML* visit = new VisitorMathML();
+	Iterator iter(ptr);
+	
+	while(!iter.is_done()) {
+		iter.current_node()->accept(visit, iter.current_index());
+		iter.next();
+	}
+
+	string result;
+	result += "<math>\n" + visit->getString() + "</math>";
+	delete visit;
+	return result;
+}
+
+#endif // __PRINT_HPP__
